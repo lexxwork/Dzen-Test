4d63d64992cacee7605d8e0a2b341dfec0b0d8f3
lexxwork <lexxwork@gmail.com>
2023-01-12 20:59:30 +0200

    fix bug

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
-import { PaginationQuery } from './paginationQuery.service';
-import { Logger } from '@nestjs/common';
-
 @Module({
   controllers: [PostController],
-  providers: [PostService, PaginationQuery, Logger],
+  providers: [PostService],
 })
 export class PostModule {}
